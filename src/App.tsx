76398efca38e40e478eb503089246e89cfe2de07
Made Baruna <made.setia@gmail.com>
2019-07-13 07:53:58 +0700

    Add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { GlobalStyle } from './GlobalStyle';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Start</h1>
     </div>
   );
---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: rgb(232, 232, 232) !important;
+  }
+`;
